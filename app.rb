2843a352c401a7e13877e55bbcaa64ca965df216
Alistair Scott <ali.scott@gmail.com>
2017-11-06 15:59:53 +0000

    fixup! Add files

--- app.rb
+++ app.rb
@@ -10,6 +10,6 @@ get '/' do
     database: ENV['MYSQL_DBNAME']
   )
   posts = client.query("SELECT * FROM posts INNER JOIN authors ON posts.author_email = authors.email ORDER BY created_at DESC")
-  @top_5_posts = posts.to_a[0..5]
+  @top_5_posts = posts.to_a[0..4]
   erb :index
 end
